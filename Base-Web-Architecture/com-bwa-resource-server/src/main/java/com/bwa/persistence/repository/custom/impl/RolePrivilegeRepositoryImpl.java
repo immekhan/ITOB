2e650cf411043838f0fef6ec9f39fd8f0716771d
immekhan <immekhan.javatech@gmail.com>
2017-05-10 10:29:09 +0500

    Updated null param in response

--- Base-Web-Architecture/com-bwa-resource-server/src/main/java/com/bwa/persistence/repository/custom/impl/RolePrivilegeRepositoryImpl.java
+++ Base-Web-Architecture/com-bwa-resource-server/src/main/java/com/bwa/persistence/repository/custom/impl/RolePrivilegeRepositoryImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RolePrivilegeRepositoryImpl implements RolePrivilegeRepositoryCustom {
@@ -14,28 +15,29 @@ public class RolePrivilegeRepositoryImpl implements RolePrivilegeRepositoryCusto
 
     @Override
     public List<String> findPrivilegeByRole(String role) {
+        List<String> privilegesList = new ArrayList<String>();
 
         if(!role.isEmpty()){
             StringBuilder sql=new StringBuilder()
                     .append("SELECT ID_PRIVILEGE FROM ITOB_ROLE_PRIVILEGES WHERE ")
                     .append("UPPER(ID_ROLE) = '")
                     .append(role.toUpperCase()).append("'");
-            return jdbcTemplate.queryForList(sql.toString(),String.class);
+            privilegesList = jdbcTemplate.queryForList(sql.toString(),String.class);
         }
-        return null;
+        return privilegesList;
     }
 
     @Override
     public List<String> findRoleByPrivilege(String privilege){
-
+        List<String> rolePrivilegesList = new ArrayList<String>();
         if(!privilege.isEmpty()){
             StringBuilder sql=new StringBuilder()
                     .append("SELECT ID_ROLE FROM ITOB_ROLE_PRIVILEGES WHERE ")
                     .append("UPPER(ID_PRIVILEGE) = '")
                     .append(privilege.toUpperCase()).append("'");
-            return jdbcTemplate.queryForList(sql.toString(),String.class);
+            rolePrivilegesList = jdbcTemplate.queryForList(sql.toString(),String.class);
         }
-        return null;
+        return rolePrivilegesList;
     }
 
 }
